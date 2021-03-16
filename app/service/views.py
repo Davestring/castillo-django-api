6ee6ff737c1a1954281ebea6d5e7b752d88b4bdd
David Martinez <davestringm@gmail.com>
2021-03-16 16:58:25 -0600

    refactor the Service views to use generics classes from rest_framework

--- app/service/views.py
+++ app/service/views.py
@@ -1,21 +1,28 @@
 """Service views module."""
 
-from typing import Optional
-
-from rest_framework import views
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 from rest_framework.request import Request
 from rest_framework.response import Response
-
-from utils.responses import CustomResponse
 
 from ..service.models import ServiceModel
 from ..service.serializers import ServiceSerializer
 
 
-class ServiceList(views.APIView):
+class ServiceList(ListCreateAPIView):
     """ServiceList.
 
     Wrapper that retrieves all the resources from the Service table.
+
+    Attributes
+    ----------
+    queryset : QuerySet
+        The queryset that should be used for returning objects from this view.
+    serializer_class : AddressSerializer
+        The serializer class that should be used for validating and
+        deserializing input, and for serializing output.
 
     Methods
     -------
@@ -26,7 +33,10 @@ class ServiceList(views.APIView):
 
     """
 
-    def get(self, request: Request) -> Response:
+    queryset = ServiceModel.objects.all()
+    serializer_class = ServiceSerializer
+
+    def get(self, request: Request, *args: tuple, **kwargs: dict) -> Response:
         """GET Request.
 
         Retrieves all the resources information of the Service table.
@@ -41,16 +51,9 @@ class ServiceList(views.APIView):
         Response
 
         """
-        queryset = ServiceModel.objects.all()
+        return self.list(request, *args, **kwargs)
 
-        services = ServiceSerializer(queryset, many=True).data
-
-        return CustomResponse(
-            data=services,
-            message="Services information as been successfully retrieved.",
-        ).success()
-
-    def post(self, request: Request) -> Response:
+    def post(self, request: Request, *args: tuple, **kwargs: dict) -> Response:
         """POST Request.
 
         Creates a new registry for the Service table.
@@ -65,28 +68,24 @@ class ServiceList(views.APIView):
         Response
 
         """
-        serializer = ServiceSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return CustomResponse(
-                data=serializer.data, message="Registry successfully created."
-            ).success()
-
-        return CustomResponse(
-            data=serializer.errors, message="Something went wrong!"
-        ).error()
+        return self.create(request, *args, **kwargs)
 
 
-class ServiceDetail(views.APIView):
+class ServiceDetail(RetrieveUpdateDestroyAPIView):
     """ServiceDetail.
 
     Wrapper that allows to perform CRUD operations to a single Service element.
 
+    Attributes
+    ----------
+    queryset : QuerySet
+        The queryset that should be used for returning objects from this view.
+    serializer_class : AddressSerializer
+        The serializer class that should be used for validating and
+        deserializing input, and for serializing output.
+
     Methods
     -------
-    _get_resource(primary_key)
-        Search for a single register in the table using the PK.
     get(request, primary_key)
         Retrieves the detailed information of a Service resource.
     put(request, primary_key)
@@ -96,28 +95,10 @@ class ServiceDetail(views.APIView):
 
     """
 
-    @classmethod
-    def _get_queryset(cls, primary_key: int) -> Optional[ServiceModel]:
-        """Get Resource.
+    queryset = ServiceModel.objects.all()
+    serializer_class = ServiceSerializer
 
-        Search for a single register in the table using the PK.
-
-        Parameters
-        ----------
-        primary_key : int
-            Resource primary key.
-
-        Returns
-        -------
-        Service if the PK exists, otherwise None.
-
-        """
-        try:
-            return ServiceModel.objects.get(pk=primary_key)
-        except ServiceModel.DoesNotExist:
-            return None
-
-    def get(self, request: Request, primary_key: int) -> Response:
+    def get(self, request: Request, *args: tuple, **kwargs: dict) -> Response:
         """GET Request.
 
         Retrieves the detailed information of a Service resource.
@@ -134,21 +115,9 @@ class ServiceDetail(views.APIView):
         Response
 
         """
-        queryset = self._get_queryset(primary_key)
+        return self.retrieve(request, *args, **kwargs)
 
-        if not queryset:
-            return CustomResponse(
-                message="The resource does not exists."
-            ).error(404)
-
-        service = ServiceSerializer(queryset).data
-
-        return CustomResponse(
-            data=service,
-            message="Information has been successfully retrieved.",
-        ).success()
-
-    def put(self, request: Request, primary_key: int) -> Response:
+    def put(self, request: Request, *args: tuple, **kwargs: dict) -> Response:
         """PUT Request.
 
         Updates a single registry of the Service table.
@@ -165,26 +134,11 @@ class ServiceDetail(views.APIView):
         Response
 
         """
-        queryset = self._get_queryset(primary_key)
+        return self.update(request, *args, **kwargs)
 
-        if not queryset:
-            return CustomResponse(
-                message="The resource does not exists."
-            ).error(404)
-
-        serializer = ServiceSerializer(queryset, data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return CustomResponse(
-                data=serializer.data, message="Registry successfully updated."
-            ).success()
-
-        return CustomResponse(
-            data=serializer.errors, message="Something went wrong!"
-        ).error()
-
-    def delete(self, request: Request, primary_key: int) -> Response:
+    def delete(
+        self, request: Request, *args: tuple, **kwargs: dict
+    ) -> Response:
         """DELETE Request.
 
         Deletes a single registry of the Service table.
@@ -201,15 +155,4 @@ class ServiceDetail(views.APIView):
         Response
 
         """
-        queryset = self._get_queryset(primary_key)
-
-        if not queryset:
-            return CustomResponse(
-                message="The resource does not exists."
-            ).error(404)
-
-        queryset.delete()
-
-        return CustomResponse(
-            message="Registry successfully deleted."
-        ).success()
+        return self.destroy(request, *args, **kwargs)
