d6f493d6e2832b244ae3e13e30b495f551a902f7
David Martinez <davestringm@gmail.com>
2021-03-16 03:37:33 -0600

    declare Service app urls

---
+++ app/service/urls.py
@@ -0,0 +1,13 @@
+"""Service urls module."""
+
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from ..service.views import ServiceDetail, ServiceList
+
+urlpatterns = [
+    path("", ServiceList.as_view(), name="service-list"),
+    path("<int:primary_key>/", ServiceDetail.as_view(), name="service-detail"),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
