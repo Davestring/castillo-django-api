22ab0e88cb7c874403ded89b5285e4e1e7b0b009
David Martinez <david@arkangeles.com>
2022-05-22 23:42:19 -0500

    chore(v2): configure api documentation view

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,14 @@
-"""app URL Configuration."""
+"""Application urls Configuration."""
 from typing import List
 
 from django.urls import URLPattern, include, path
+
+from utils import SwaggerView
 
 urlpatterns: List[URLPattern] = [
     path("", include("app.auth.urls")),
     path("", include("app.booking.urls")),
     path("", include("app.catalog.urls")),
     path("", include("app.health.urls")),
+    path("docs/", SwaggerView.with_ui("redoc", cache_timeout=0), name="docs"),
 ]
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,10 @@
 """Utils package initializer."""
 from utils.models import BaseModel
 from utils.pagination import DefaultPagination
+from utils.swagger import SwaggerView
 
 __all__ = [
     "BaseModel",
     "DefaultPagination",
+    "SwaggerView",
 ]
