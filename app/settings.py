fcde4910826d3a84d3490002c8e85e5e7e9ee99d
David Martinez <dhernandez@applaudostudios.com>
2022-10-14 03:34:01 -0500

    fix: lint issues

--- app/settings.py
+++ app/settings.py
@@ -21,16 +21,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-    'x-api-key',
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "x-api-key",
 ]
 
 DEFAULT_MAX_PAGE_SIZE = 20000
